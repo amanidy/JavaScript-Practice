b1afb4e4496e775b0d6bb243280976f64faac20b
amanidy <amaniarnold08@gmail.com>
2026-01-29 17:47:03 +0300

    Updated score list Manager

---
+++ 30-day JavaScript improvement plan/array.js
@@ -0,0 +1,13 @@
+function manageScores(){
+  let scores =[59,70,42,98];
+  
+  scores.push(56);
+  console.log(scores);
+  
+  console.log(scores[2]);
+  
+  scores.pop();
+  console.log(scores);
+}
+
+manageScores();
