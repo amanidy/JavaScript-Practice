814441c47d5d548081abe4e989482d09f54d6f0a
amanidy <amaniarnold08@gmail.com>
2026-02-15 10:47:45 +0300

    update script.js

---
+++ 30-day JavaScript improvement plan/Final Project /js/script.js
@@ -0,0 +1,13 @@
+function createResource() {
+  const resourceArr =[
+    {
+      "id":1,
+      "name":"Food",
+      "quality":8,
+      "location":"Wundanyi",
+      "expire_date":14/02/2026
+    }
+  ];
+  
+  return resourceArr[0].quality;
+}
