6cb9a506160b11adcdcfa8c1c263e1684a5afb7a
amanidy <amaniarnold08@gmail.com>
2026-02-15 16:45:10 +0300

    Defined resources data structure

--- 30-day JavaScript improvement plan/Final Project /js/script.js
+++ 30-day JavaScript improvement plan/Final Project /js/script.js
@@ -1,13 +1,27 @@
 function createResource() {
-  const resourceArr =[
+  const resources =[
     {
       "id":1,
-      "name":"Food",
+      "title":"Food",
       "quality":8,
-      "location":"Wundanyi",
+      "location":"Nyeri",
       "expire_date":14/02/2026
+    },
+    {
+      "id":2,
+      "title":"Water",
+      "quality":10,
+      "location":"Muranga",
+      "expire_date":14/03/2026
+    },
+    {
+      "id":3,
+      "title":"Kirinyaga",
+      "quality":9,
+      "location":"Wundanyi",
+      "expire_date":14/04/2026
     }
   ];
   
-  return resourceArr[0].quality;
+  return resources;
 }
