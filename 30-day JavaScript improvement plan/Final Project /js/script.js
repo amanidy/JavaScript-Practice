227f775f3cb33500826587d20ce40eb1e99e0258
amanidy <amaniarnold08@gmail.com>
2026-02-15 17:12:07 +0300

    Added a new resource to the resource data

--- 30-day JavaScript improvement plan/Final Project /js/script.js
+++ 30-day JavaScript improvement plan/Final Project /js/script.js
@@ -3,25 +3,44 @@ function createResource() {
     {
       "id":1,
       "title":"Food",
-      "quality":8,
+      "quantity":8,
       "location":"Nyeri",
-      "expire_date":14/02/2026
+      "expire_date":"14/02/2026"
     },
     {
       "id":2,
       "title":"Water",
-      "quality":10,
+      "quantity":10,
       "location":"Muranga",
-      "expire_date":14/03/2026
+      "expire_date":"14/03/2026"
     },
     {
       "id":3,
-      "title":"Kirinyaga",
-      "quality":9,
-      "location":"Wundanyi",
-      "expire_date":14/04/2026
+      "title":"Medicine",
+      "quantity":9,
+      "location":"Kirinyaga",
+      "expire_date":"14/04/2026"
     }
   ];
   
+  addResource(resources,4,"Clothes",1000,"Nairobi","14/05/2026")
+  
   return resources;
+}
+
+
+function addResource(resources,id,title, quantity,location,expiry_date){
+  
+  
+  
+  resources.push({
+    "id":id,
+    "title":title,
+    "quantity":quantity,
+    "location": location,
+    "expiry_date":expiry_date
+  })
+  
+  return resources;
+  
 }
